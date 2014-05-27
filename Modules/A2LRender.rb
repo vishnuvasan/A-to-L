289c15d09588c63efe397d79210fc871944423b0
Vishnu Vasan <vishnuvasan@vishnuvasan.com>
2014-05-28 01:46:58 +0530

    Combined the Interpolation Compu Method Function to Generic Compu Method Renderer

--- Modules/A2LRender.rb
+++ Modules/A2LRender.rb
@@ -49,17 +49,56 @@ module A2LRender
       if((inputs[key]["COMPU_METHOD_TYPE"].upcase=="RAT_FUNC") or (inputs[key]["COMPU_METHOD_TYPE"].upcase=="RATIONAL") or (inputs[key]["COMPU_METHOD_TYPE"].upcase=="RAT")) then
         inputs[key]["COMPU_METHOD_TYPE"]="RAT_FUNC"
         inputs[key]["COEFF_TYPE"]="COEFFS"
+        Log._Validate(inputs,"RAT_FUNC")
         Mustache.template_file=File.dirname(__FILE__) + Templates + "CompuMethod-Rational.mustache"
       end      
 
       #Compu Method Type ==> FORM
-      if(inputs[key]["COMPU_METHOD_TYPE"].upcase=="FORM") then
-        inputs[key]["COMPU_METHOD_TYPE"]=inputs[key]["COMPU_METHOD_TYPE"].upcase
+      if((inputs[key]["COMPU_METHOD_TYPE"].upcase=="FORM") or (inputs[key]["COMPU_METHOD_TYPE"].upcase=="FORMULA")) 
+        inputs[key]["COMPU_METHOD_TYPE"]="FORM"
+        Log._Validate(inputs,"FORM")
         Mustache.template_file=File.dirname(__FILE__) + Templates + "CompuMethod-Form.mustache"        
       end
 
 
+      #Compu Method Type ==> INTERPOLATION
+      if(inputs[key]["COMPU_METHOD_TYPE"].upcase=="TAB_INTP") then  
 
+        if(inputs[key]["INTERPOLATION"]=="ENABLE") then
+          inputs[key]["COMPU_TAB_TYPE"]="TAB_INTP"
+        elsif(inputs[key]["INTERPOLATION"]=="DISABLE") then     
+          inputs[key]["COMPU_TAB_TYPE"]="TAB_NOINTP"
+        end
+
+        if defined? inputs[key]["INTERPOLATION_VALUES"] then
+          puts inputs[key]["INTERPOLATION_SIZE"]=inputs[key]["INTERPOLATION_VALUES"].length
+        else
+          inputs[key]["INTERPOLATION_SIZE"]=""
+        end
+
+        if defined? inputs[key]["INTERPOLATION_VALUES"] then
+          values=""
+          desired_size=0
+          actual_size=inputs[key]["INTERPOLATION_VALUES"].length
+          for index in inputs[key]["INTERPOLATION_VALUES"].keys
+            desired_size+=1
+            if desired_size==1 then pad_space="" else pad_space="    " end
+            if desired_size==actual_size then pad_line="" else pad_line="\n" end
+            values+=pad_space+index.to_s + " "
+            values+=inputs[key]["INTERPOLATION_VALUES"][index].to_s + pad_line
+          end 
+          inputs[key]["INTERPOLATION_VALUES"]=values
+        end
+
+        if(inputs[key]["DEFAULT_VALUE"]==nil) or (inputs[key]["DEFAULT_VALUE"]=="") then
+          inputs[key]["DEFAULT_VALUE"]=""
+        elsif inputs[key]["DEFAULT_VALUE"]!=nil then
+          inputs[key]["DEFAULT_VALUE"]="DEFAULT_VALUE_NUMERIC " + inputs[key]["DEFAULT_VALUE"].to_s
+        end
+
+        Log._Validate(inputs,"TAB_INTP")
+        Mustache.template_file=File.dirname(__FILE__) + Templates + "CompuMethod-TabIntp.mustache"         
+      end    
 
 
       #By Default the Compu Method Type would be RAT_FUNC.
@@ -75,70 +114,6 @@ module A2LRender
       total+=Mustache.render(A2LRender._to_s(inputs[key]))
   	
     end
-    return total
-  end
-
-  # def A2LRender._CompuMethodForm(inputs)
-  #   Mustache.template_file=File.dirname(__FILE__) + Templates + "CompuMethod-Form.mustache"
-  #   total=""
-  #   for key in inputs.keys
-
-  #     #By Default the Compu Method Type would be FORM.
-  #     if((inputs[key]["COMPU_METHOD_TYPE"]=="")or(inputs[key]["COMPU_METHOD_TYPE"]==nil)) then 
-  #       inputs[key]["COMPU_METHOD_TYPE"]="FORM" 
-  #     end
-      
-  #     total+=Mustache.render(A2LRender._to_s(inputs[key]))
-    
-  #   end
-  #   return total
-  # end
-
-  def A2LRender._CompuMethodIntp(inputs)
-    Mustache.template_file=File.dirname(__FILE__) + Templates + "CompuMethod-TabIntp.mustache"
-    total=""
-    for key in inputs.keys
-
-      #By Default the Compu Method Type would be TAB_INTP.
-      if((inputs[key]["COMPU_METHOD_TYPE"]=="")or(inputs[key]["COMPU_METHOD_TYPE"]==nil)) then 
-        inputs[key]["COMPU_METHOD_TYPE"]="TAB_INTP" 
-      end
-
-      if(inputs[key]["INTERPOLATION"]=="ENABLE") then
-        inputs[key]["COMPU_TAB_TYPE"]="TAB_INTP"
-      elsif(inputs[key]["INTERPOLATION"]=="DISABLE") then     
-        inputs[key]["COMPU_TAB_TYPE"]="TAB_NOINTP"
-      end
-
-      if defined? inputs[key]["INTERPOLATION_VALUES"] then
-        puts inputs[key]["INTERPOLATION_SIZE"]=inputs[key]["INTERPOLATION_VALUES"].length
-      else
-        inputs[key]["INTERPOLATION_SIZE"]=""
-      end
-
-      if defined? inputs[key]["INTERPOLATION_VALUES"] then
-        values=""
-        desired_size=0
-        actual_size=inputs[key]["INTERPOLATION_VALUES"].length
-        for index in inputs[key]["INTERPOLATION_VALUES"].keys
-          desired_size+=1
-          if desired_size==1 then pad_space="" else pad_space="    " end
-          if desired_size==actual_size then pad_line="" else pad_line="\n" end
-          values+=pad_space+index.to_s + " "
-          values+=inputs[key]["INTERPOLATION_VALUES"][index].to_s + pad_line
-        end 
-        inputs[key]["INTERPOLATION_VALUES"]=values
-      end
-
-      if(inputs[key]["DEFAULT_VALUE"]==nil) or (inputs[key]["DEFAULT_VALUE"]=="") then
-        inputs[key]["DEFAULT_VALUE"]=""
-      elsif inputs[key]["DEFAULT_VALUE"]!=nil then
-        inputs[key]["DEFAULT_VALUE"]="DEFAULT_VALUE_NUMERIC " + inputs[key]["DEFAULT_VALUE"].to_s
-      end
-      
-      total+=Mustache.render(A2LRender._to_s(inputs[key]))
-    end
-    
     return total
   end  
 
