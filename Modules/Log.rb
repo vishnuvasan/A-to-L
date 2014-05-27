27b21f4aa92b991b72c8b8992a8c8a3204bb0052
Vishnu Vasan <vishnuvasan@vishnuvasan.com>
2014-05-28 01:47:44 +0530

    Added the Necessary Log Information with Regard to Interpolation Compu Methods

--- Modules/Log.rb
+++ Modules/Log.rb
@@ -18,6 +18,7 @@ module Log
 					is_name=is_description=is_compu_method_type=is_physical_rep=is_unit="VALID"
 					is_coeff_type=is_coeff_a=is_coeff_b=is_coeff_c=is_coeff_d=is_coeff_e=is_coeff_f=""
 					is_formula=is_formula_inv=""
+					is_compu_tab_ref=is_compu_tab_description=is_interpolation=is_interpolation_values=""
 				end
 
 				if (template.to_s).upcase=="LINEAR"
@@ -25,13 +26,30 @@ module Log
 					is_coeff_type=is_coeff_a=is_coeff_b="VALID"
 					is_coeff_c=is_coeff_d=is_coeff_e=is_coeff_f=""
 					is_formula=is_formula_inv=""
+					is_compu_tab_ref=is_compu_tab_description=is_interpolation=is_interpolation_values=""
 				end
 
 				if (template.to_s).upcase=="RAT_FUNC"
 					is_name=is_description=is_compu_method_type=is_physical_rep=is_unit="VALID"
 					is_coeff_type=is_coeff_a=is_coeff_b=is_coeff_c=is_coeff_d=is_coeff_e=is_coeff_f="VALID"
 					is_formula=is_formula_inv=""
-				end				
+					is_compu_tab_ref=is_compu_tab_description=is_interpolation=is_interpolation_values=""
+				end	
+
+				if (template.to_s).upcase=="FORM"
+					is_name=is_description=is_compu_method_type=is_physical_rep=is_unit="VALID"
+					is_coeff_type=is_coeff_a=is_coeff_b=is_coeff_c=is_coeff_d=is_coeff_e=is_coeff_f=""
+					is_formula=is_formula_inv="VALID"
+					is_compu_tab_ref=is_compu_tab_description=is_interpolation=is_interpolation_values=""
+				end					
+
+				if (template.to_s).upcase=="TAB_INTP"
+					is_name=is_description=is_compu_method_type=is_physical_rep=is_unit="VALID"
+					is_coeff_type=is_coeff_a=is_coeff_b=is_coeff_c=is_coeff_d=is_coeff_e=is_coeff_f=""
+					is_formula=is_formula_inv=""
+					is_compu_tab_ref=is_compu_tab_description=is_interpolation=is_interpolation_values="VALID"
+					is_default_value="VALID"					
+				end			
 
 				if defined? inputs[key]["NAME"]
 				  log.puts _Format_Line("NAME",_Format_Line(is_name,inputs[key]["NAME"].to_s))
@@ -88,6 +106,26 @@ module Log
 				if defined? inputs[key]["FORMULA_INV"]
 				  log.puts _Format_Line("FORMULA_INV",_Format_Line(is_formula_inv,inputs[key]["FORMULA_INV"].to_s))
 				end
+
+				if defined? inputs[key]["COMPU_TAB_REF"]
+				  log.puts _Format_Line("COMPU_TAB_REF",_Format_Line(is_compu_tab_ref,inputs[key]["COMPU_TAB_REF"].to_s))
+				end
+
+				if defined? inputs[key]["COMPU_TAB_DESCRIPTION"]
+				  log.puts _Format_Line("COMPU_TAB_DESCRIPTION",_Format_Line(is_compu_tab_description,inputs[key]["COMPU_TAB_DESCRIPTION"].to_s))
+				end
+
+				if defined? inputs[key]["INTERPOLATION"]
+				  log.puts _Format_Line("INTERPOLATION",_Format_Line(is_interpolation,inputs[key]["INTERPOLATION"].to_s))
+				end				
+
+				if defined? inputs[key]["INTERPOLATION_VALUES"]
+				  log.puts _Format_Line("INTERPOLATION_VALUES",_Format_Line(is_interpolation_values,inputs[key]["INTERPOLATION_VALUES"].to_s))
+				end		
+
+				if defined? inputs[key]["DEFAULT_VALUE"]
+				  log.puts _Format_Line("DEFAULT_VALUE",_Format_Line(is_default_value,inputs[key]["DEFAULT_VALUE"].to_s))
+				end		
 
 			end
 	  	end
